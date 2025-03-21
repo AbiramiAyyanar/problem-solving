f075eae0d58f4f86c9800d589fa3f9f75d4e7848
AbiramiAyyanar <abiayyanar01@gmail.com>
2025-03-21 15:30:53 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,100 @@
+# a = 1475
+# b = 5145
+# print(a + b)
+# c = a + b
+
+# for i in range (c):
+#     print(i)
+
+# a="I Am EATing"
+# print(a.swapcase())
+# a="I Am EATing"
+# print(a.capitalize())
+# a="i am eating"
+# print(a.count(""))
+
+# a="hello"
+# print[::-1]
+
+# a=int(input("enter a number:"))
+# if a>5:
+#     print("f{a}this number bigger than 5:")
+# else:
+#     print("this number is smaller")
+
+# n=int(input("enter s number:"))
+# if n%2==0:
+
+
+# num = int(input('Enter a number: '))
+# while num != 5 :
+#   print(num, num * num)
+#   break
+
+# count = 1
+# while count < 10 :
+#   print(count, count * count, count * count * count)
+# count += 1
+
+# n=int(input("enter a number:"))
+# print(n)
+
+
+# i=1
+# while i<=10:
+#     print("number",i)
+#     i=i+1
+# print("stop")
+
+# i=20
+# while i<=2:
+#     print(i)
+#     i=i-1
+
+# i=20
+# while i<=2:
+#     print(i)
+#     i=i+1
+# n=0
+# while n<=6:
+#     print(n)
+#     n=n+1
+# print("end")
+
+# n="abirami"
+# for i in n:
+#     print(i)
+
+# swap the variable
+# x=5
+# y=7
+# tem=y
+# tem=x
+# print("the x value is:",y)
+# print("the y value is:",x)
+
+# even odd program
+# n=int(input("enter a number:"))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+# n="abirami"[::-1]
+# print(n)
+
+# string="abirami"
+# print(string[::-1])
+
+# my_list = [hello world]
+# result = ' '.join(my_list)  # Using space as separator
+# print(result)  # Output: Hello World
+# palinromefoi
+# n=input("enter a string:")
+# if n==n[::-1]:
+#     print("yes")
+# else:
+#     print("no")
+
+number=[1,2,3,4,5]
+string_numbers=(str(num)for num in range)
+print(string_numbers)
